e85e347dd03a9197d935233f092a561c146671f1
HOVOH <71901758+HOVOH@users.noreply.github.com>
2022-02-11 22:17:42 -0500

    add starter file for test

---
+++ test/starter.test.ts
@@ -0,0 +1,28 @@
+import { MockContract, MockContractFactory, smock } from "@defi-wonderland/smock";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import hre from "hardhat";
+
+import "../chai-setup";
+import { IExodiaContractsRegistry } from "../src/contracts/exodiaContracts";
+import { IExtendedHRE } from "../src/HardhatRegistryExtension/ExtendedHRE";
+const xhre = hre as IExtendedHRE<IExodiaContractsRegistry>;
+const { deployments, get, getNamedAccounts, getUnnamedAccounts } = xhre;
+
+describe("Start test file", function () {
+    let deployer: SignerWithAddress;
+    let otherAccount: SignerWithAddress;
+
+    // Use a fixture to deploy new contracts to speed up testing time
+    const setup = deployments.createFixture(async (hh) => {});
+
+    beforeEach(async function () {
+        const { deployer: deployerAddress } = await getNamedAccounts();
+        const [address0] = await getUnnamedAccounts();
+        deployer = await xhre.ethers.getSigner(deployerAddress);
+        otherAccount = await xhre.ethers.getSigner(address0);
+        await setup();
+    });
+});
