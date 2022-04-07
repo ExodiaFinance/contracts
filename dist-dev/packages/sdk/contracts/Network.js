49e1b198b86ccdbc09162e9aa90b35146e237e15
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-04-07 12:35:21 +1000

    Delete dist-dev directory

--- dist-dev/packages/sdk/contracts/Network.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Network = void 0;
-var Network;
-(function (Network) {
-    Network[(Network["OPERA_MAIN_NET"] = 250)] = "OPERA_MAIN_NET";
-    Network[(Network["OPERA_TEST_NET"] = 4002)] = "OPERA_TEST_NET";
-    Network[(Network["HARDHAT"] = 31337)] = "HARDHAT";
-})((Network = exports.Network || (exports.Network = {})));
